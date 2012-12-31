4abb94f3bdfaadf87770e13d93381b475d7bc674
Beyamor <beyamor@gmail.com>
2012-12-31 11:40:47 -0800

    Cool. Patterns works.

--- markovenizer/src/markovenizer/core.clj
+++ markovenizer/src/markovenizer/core.clj
@@ -2,4 +2,9 @@
 
 (defn patterns
   [pattern-length text]
-  [[[] (first text)]])
+  (for [result-index (-> text count range)]
+    (let [resulting-char (get text result-index)
+          pattern (.substring text
+                              (max (- result-index pattern-length) 0)
+                              result-index)]
+      [pattern resulting-char])))
--- markovenizer/test/markovenizer/test/core.clj
+++ markovenizer/test/markovenizer/test/core.clj
@@ -3,4 +3,9 @@
   (:use [clojure.test]))
 
 (deftest can-generate-patterns
-         (is (= [[[] \a]] (patterns 2 "a"))))
+         (is (= [["" \a]] (patterns 2 "a")))
+         (is (= [["" \a] ["a" \b]] (patterns 2 "ab")))
+         (is (= [["" \a] ["a" \b] ["ab" \c]] (patterns 2 "abc")))
+         (is (= [["" \a] ["a" \b] ["ab" \c] ["bc" \d]] (patterns 2 "abcd")))
+         (is (= [["" \a] ["a" \b] ["b" \c] ["c" \d]] (patterns 1 "abcd")))
+         (is (= [["" \a] ["a" \b] ["ab" \c] ["abc" \d]] (patterns 3 "abcd"))))
