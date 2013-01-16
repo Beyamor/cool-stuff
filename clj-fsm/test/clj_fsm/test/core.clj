19972c0eb1405ddbb4ee447cddb2027b926c1c53
Beyamor <beyamor@gmail.com>
2013-01-15 16:39:11 -0800

    "fsm-state" to "fsm"

--- clj-fsm/test/clj_fsm/test/core.clj
+++ clj-fsm/test/clj_fsm/test/core.clj
@@ -3,23 +3,23 @@
   (:use [clojure.test]))
 
 (deftest can-create-single-state-machine
-  (let [fsm-state (fsm
+  (let [fsm (fsm
                     {:initial :only-state
                      :states {:only-state {:action (constantly :result)
                                            :transitions {:only-state (constantly true)}}}})
-        result (act fsm-state)]
+        result (act fsm)]
     (is (= :result result))))
 
 (deftest can-create-a-cyclic-state-machine
- (let [fsm-state (fsm
+ (let [fsm (fsm
                    {:initial :a
                     :states {:a {:action (constantly :a)
                                  :transitions {:b (constantly true)}}
                              :b {:action (constantly :b)
                                  :transitions {:a (constantly true)}}}})
-       res1 (act fsm-state)
-       fsm-state (transition fsm-state)
-       res2 (act fsm-state)
-       fsm-state (transition fsm-state)
-       res3 (act fsm-state)]
+       res1 (act fsm)
+       fsm (transition fsm-state)
+       res2 (act fsm)
+       fsm (transition fsm-state)
+       res3 (act fsm)]
    (is (= [:a :b :a] [res1 res2 res3]))))
