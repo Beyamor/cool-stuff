e6ace1f6dc47079d4b70f12d135efa2184cc3569
Beyamor <beyamor@gmail.com>
2013-01-16 17:43:35 -0800

    Multimethooooooods

--- clj-fsm/src/clj_fsm/core.clj
+++ clj-fsm/src/clj_fsm/core.clj
@@ -1,52 +1,85 @@
 (ns clj-fsm.core)
 
-(defn- get-state-action
-  [{:keys [action]}]
-  action)
+(defmulti machine-property (fn [table property data] property))
+(defmulti state-property (fn [table property data] property))
 
-(defn- get-transitions-fn
-  [{:keys [transitions]}]
-  {:pre [(not (nil? transitions))]}
-  (fn [& args]
-    {:post [(not (nil? %))]}
-    (some
-      (fn [[next-state transition-predicate]]
-        (if (apply transition-predicate args) next-state))
-      transitions)))
+(defn add-to-state
+  [table state data]
+  {:pre [(map? data)]}
+  (swap! table update-in [state] merge data))
 
-(defn- get-next-state-fn
-  [state-name state-spec]
-  (cond
-    (contains? state-spec :next-state) (constantly (state-spec :next-state))
-    (contains? state-spec :transitions) (get-transitions-fn state-spec)
-    :else (constantly state-name)))
-
-(defn- get-state-transitions
-  [state-name state-spec state-table]
-  (let [next-state-fn (get-next-state-fn state-name state-spec)]
-    (fn [& args]
-      {:post [(not (nil? %))]}
-      (@state-table (apply next-state-fn args)))))
+(defn initial-state
+  [table]
+  {:pre [(map? table)]
+   :post [(map? %)]}
+  (->>
+    table
+    (map (fn [[_ state-data]]
+           (if (:initial state-data) state-data)))
+    (filter identity)
+    first))
 
 (defn fsm
-  [spec]
-  (let [state-table (atom {})
-        state-specs (:states spec)
-        initial-state-name (:initial spec)]
-    (dorun
-      (map
-        (fn [[state-name state-spec]]
-          (let [state-action (get-state-action state-spec)
-                state-transitions (get-state-transitions state-name state-spec state-table)
-                state {:action state-action, :transition state-transitions}]
-            (swap! state-table assoc state-name state)))
-        state-specs))
-    (@state-table initial-state-name)))
+  [machine-spec]
+  (let [table (atom {})]
+    (dorun (map (fn [[property data]] (machine-property table property data)) machine-spec))
+    (initial-state @table)))
+
+(defmethod machine-property
+  :initial
+  [table _ initial-state]
+  (add-to-state table initial-state {:initial true}))
+
+(defn- state-data
+  [table state-spec]
+  (reduce
+    merge {}
+    (map
+      (fn [[property data]]
+        (state-property table property data))
+      state-spec)))
+
+(defmethod machine-property
+  :states
+  [table _ state-specs]
+  (dorun
+    (map
+      (fn [[state state-spec]]
+        (add-to-state
+          table state (state-data table state-spec)))
+      state-specs)))
+
+(defmethod state-property
+  :action
+  [table _ action]
+  {:action action})
+
+(defmethod state-property
+  :next-state
+  [table _ next-state]
+  {:transition (fn [& _] (@table next-state))})
+
+(defn- next-state-fn
+  [transitions]
+  (fn [& args]
+    {:post [%]}
+     (some
+       (fn [[next-state transition-predicate]]
+         (if (apply transition-predicate args) next-state))
+       transitions)))
+
+(defmethod state-property
+  :transitions
+  [table _ transitions]
+  {:transition (fn [& args]
+                 (@table (apply (next-state-fn transitions) args)))})
 
 (defn act
-  [{:keys [action]} & args]
-  (apply action args))
+  [state & args]
+  {:pre [(:action state)]}
+  (apply (state :action) args))
 
 (defn transition
-  [{:keys [transition]} & args]
-  (apply transition args))
+  [state & args]
+  {:pre [(:transition state)]}
+  (apply (state :transition) args))
