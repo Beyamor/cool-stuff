a4ab86efd043e1bbdb062375dffa6d1a35ee4d4e
Beyamor <beyamor@gmail.com>
2013-05-04 16:57:17 -0700

    Creating a frame in main

--- linden/src/linden/core.clj
+++ linden/src/linden/core.clj
@@ -1,5 +1,6 @@
 (ns linden.core
-  (:use [clojure.string :only [split trim]]))
+  (:use [clojure.string :only [split trim]]
+        seesaw.core))
 
 (defn parse-rule
   [input]
@@ -7,3 +8,15 @@
         pred (trim pred)
         succ (->> succ (map trim) (remove empty?))]
     [pred succ]))
+
+(defn -main
+  [& args]
+  (invoke-later
+    (->
+      (frame
+        :title "L-Systems"
+        :content "L-Systems go!"
+        :minimum-size [800 :by 600]
+        :on-close :exit)
+      pack!
+      show!)))
