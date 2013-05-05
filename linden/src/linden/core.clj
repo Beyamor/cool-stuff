03587c60c9a3027a8e5f008d290922c6a7599a41
Beyamor <beyamor@gmail.com>
2013-05-04 18:36:24 -0700

    Parsing a rule block

--- linden/src/linden/core.clj
+++ linden/src/linden/core.clj
@@ -1,9 +1,13 @@
 (ns linden.core
-  (:use [clojure.string :only [split trim]]))
+  (:use [clojure.string :only [split trim split-lines]]))
 
 (defn parse-rule
   [input]
-  (let [[pred succ] (split input #"->")
-        pred (trim pred)
-        succ (->> succ (map trim) (remove empty?))]
-    [pred succ]))
+  (let [[pred succ] (split input #"->")]
+    {:pred (trim pred)
+     :succ (->> succ (map trim) (remove empty?))}))
+
+(defn parse-rule-block
+  "Reads a bunch of newline-separated rules and spits out a sequence of rules."
+  [rule-block]
+  (->> rule-block split-lines (map parse-rule)))
--- linden/test/linden/core_test.clj
+++ linden/test/linden/core_test.clj
@@ -4,5 +4,16 @@
 
 (deftest can-parse-rules
          (are [input output] (= (parse-rule input) output)
-              "F -> F-F+FF" ["F" ["F" "-" "F" "+" "F" "F"]]
-              "F  -> F - F  +F F" ["F" ["F" "-" "F" "+" "F" "F"]]))
+              "F -> F-F+FF" {:pred "F"
+                             :succ ["F" "-" "F" "+" "F" "F"]}
+              "F  -> F - F  +F F" {:pred "F"
+                                   :succ ["F" "-" "F" "+" "F" "F"]}))
+
+(deftest can-parse-rule-blocks
+         (is (= [{:pred "F"
+                  :succ ["F" "-" "F"]}
+                 {:pred "F"
+                  :succ ["F" "F" "-" "F"]}]
+                (parse-rule-block
+                  "F -> F-F
+                   F -> FF-F"))))
