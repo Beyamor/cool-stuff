7d5803a488eee28eaef3275a88322e3d0ee34e06
Beyamor <beyamor@gmail.com>
2013-05-04 17:29:52 -0700

    Drawing a form

--- linden/src/linden/core.clj
+++ linden/src/linden/core.clj
@@ -11,6 +11,29 @@
         succ (->> succ (map trim) (remove empty?))]
     [pred succ]))
 
+(defn draw-form
+  [graphics instructions
+   & {:keys [step-size angle-increment initial-x initial-y]
+      :or {step-size 20 angle-increment 90 initial-x 300 initial-y 200}}]
+
+  (.setColor graphics (color :black))
+
+  (loop [x initial-x, y initial-y, angle 90, instructions instructions]
+    (when (seq instructions)
+      (let [[instruction & more-instructions] instructions]
+        (case instruction
+          "F"
+          (let [next-x (+ x (-> angle Math/toRadians Math/cos (* step-size)))
+                next-y (+ y (-> angle Math/toRadians Math/sin (* -1 step-size)))]
+            (.drawLine graphics x y next-x next-y)
+            (recur next-x next-y angle more-instructions))
+
+          "-"
+          (recur x y (- angle angle-increment) more-instructions)
+
+          "+"
+          (recur x y (+ angle angle-increment) more-instructions))))))
+
 (defn -main
   [& args]
   (invoke-later
@@ -24,7 +47,10 @@
                              :paint (fn [this graphics]
                                       (draw graphics
                                             (rect 0 0 600 400)
-                                            (style :background :white))))])
+                                            (style :background :white))
+                                      (draw-form
+                                        graphics
+                                        ["F" "F" "F" "-" "F" "F" "-" "F" "-" "F" "+" "F" "+" "F" "F" "-" "F" "-" "F" "F" "F"])))])
         :minimum-size [800 :by 600]
         :on-close :exit)
       pack!
