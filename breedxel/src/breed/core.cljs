ff87c66cf9a37b09f8ff654be9835436f306cb23
beyamor <beyamor@gmail.com>
2013-10-13 12:46:34 -0700

    Sets of xels

--- breedxel/src/breed/core.cljs
+++ breedxel/src/breed/core.cljs
@@ -11,6 +11,15 @@
                        y (range height)]
                    [[x y] (rand-nth colors)]))})
 
+(defn new-xel-set
+  [& {:keys [width height xel-width xel-height colors]}]
+  {:width width
+   :height height
+   :xels (into {}
+               (for [i (range width)
+                     j (range height)]
+                 [[i j] (new-xel xel-width xel-height colors)]))})
+
 (defn draw-xel!
   [canvas {xel-width :width xel-height :height :as xel}
    & {:keys [x y width height border]
@@ -28,14 +37,37 @@
           :height cell-height
           :color (get-in xel [:cells [i j]]))))))
 
+(defn draw-xels!
+  [canvas  {xels-width :width xels-height :height :as xels}
+   & {:keys [x y width height border]
+      :or {x 0 y 0 border 0}}]
+  (let [xel-width (/ width xels-width)
+        xel-height (/ height xels-height)]
+    (doseq [i (range xels-width)
+            j (range xels-height)]
+      (doto canvas
+        (draw-xel! (get-in xels [:xels [i j]])
+                  :x (+ x (* i xel-width))
+                  :y (+ y (* j xel-height))
+                  :width xel-width
+                  :height xel-height
+                  :border border)))))
+
 (defn run
   []
   (let [canvas (cnvs/create :width 600 :height 600
                             :clear-color "pink"
                             :parent "#app")
-        xel (new-xel 32 32 #{"black" "white"})]
+        xel-width 3
+        xel-height 3
+        xels (new-xel-set
+               :width 3
+               :height 3
+               :xel-width 32
+               :xel-height 32
+               :colors #{"black" "white"})]
     (doto canvas
       cnvs/clear!
-      (draw-xel! xel :x 0 :y 0 :height 600 :width 600 :border 20))))
+      (draw-xels! xels :width 600 :height 600 :border 10))))
 
 ($ run)
