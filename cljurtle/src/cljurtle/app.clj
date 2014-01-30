0123603105d5259e3bd3328b856314d74b670c8d
beyamor <beyamor@gmail.com>
2014-01-30 13:56:47 -0800

    Whoa, actually evaling a script

--- cljurtle/src/cljurtle/app.clj
+++ cljurtle/src/cljurtle/app.clj
@@ -42,9 +42,56 @@
                     (s/repaint! el)))
     el))
 
+(defn interns
+  [ns & interns]
+  (doseq [[name val] (partition 2 interns)]
+    (intern ns name val)))
+
+(defn set-up-script-ns
+  [script-ns turtles]
+  (binding [*ns* script-ns]
+    (eval '(do
+             (clojure.core/refer-clojure)
+             (require '[lonocloud.synthread :as ->])
+             (require '[cljurtle.core :as core
+                        :refer [forward back left right new-turtle pen-color jump-to]]))))
+  (intern script-ns 'def-turtle (fn [turtle]
+                                  (swap! turtles conj turtle))))
+
+(defn eval-script
+  [script]
+  (let [script-ns-name  (gensym)
+        script-ns       (create-ns script-ns-name)
+        turtles         (atom [])
+        prev-ns         *ns*]
+    (set-up-script-ns script-ns turtles)
+    (binding [*ns*  script-ns]
+      (-> (str "(do " script ")")
+        read-string
+        eval))
+    (remove-ns script-ns-name)
+    (first @turtles)))
+
 (defn run
   [state script]
-  (println script))
+  (reset! state
+          (eval-script
+            "(defn fib
+              [turtle depth]
+                (-> turtle
+                  (forward 30)
+                  (->/when (> depth 2)
+                    (left 15)
+                    (fib (- depth 1))
+                    (right 30)
+                    (fib (- depth 2))
+                    (left 15))
+                    (back 30)))
+
+            (def-turtle
+              (-> (new-turtle 0 -100)
+                (pen-color \"green\")
+                (fib 10)))")))
 
 (defn -main [& args]
   (let [turtle      (atom nil)
@@ -65,10 +112,4 @@
                                          run-button])
                    :on-close  :exit)
         s/pack!
-        s/show!))
-    (s/invoke-later
-      (reset! turtle
-              (-> (new-turtle 0 -100)
-                (pen-color "green")
-                (fib 10))))
-    nil))
+        s/show!))))
