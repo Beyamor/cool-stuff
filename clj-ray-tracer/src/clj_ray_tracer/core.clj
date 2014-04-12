19d17673ecb29d6b8fbdd788d8127b014c82c1c0
beyamor <beyamor@gmail.com>
2014-04-12 13:13:05 -0700

    Hey, crappily tracing a scene

--- clj-ray-tracer/src/clj_ray_tracer/core.clj
+++ clj-ray-tracer/src/clj_ray_tracer/core.clj
@@ -1,17 +1,20 @@
 (ns clj-ray-tracer.core
+  (:use [clojure.java.io :only [file]])
   (:require [euclidean.math.vector :as v])
-  (:import java.awt.Color))
+  (:import java.awt.Color
+           java.awt.image.BufferedImage
+           javax.imageio.ImageIO))
 
 (defn v3
   [x y z]
   (v/vector x y z))
 
-(defn sphere
+(defn create-sphere
   [center radius]
   {:center center
    :radius radius})
 
-(defn ray
+(defn create-ray
   [point direction]
   {:point point
    :direction (v/normalize direction)})
@@ -41,3 +44,54 @@
           (neg? t1) nil
           (neg? t0) t1
           :else t0) t0))))
+
+(defn collision-info
+  [ray {:keys [shape] :as object}]
+  (when-let [t (intersection shape ray)]
+    {:t t
+     :object object}))
+
+(defn find-collision
+  [ray objects]
+  (->> objects
+      (map #(collision-info ray %))
+      (filter identity)
+      (sort-by :t)
+      first
+      :object))
+
+(def scene
+  {:eye (v3 0 0 20)
+   :objects [{:shape (create-sphere (v3 0 0 -20) 5)
+              :color Color/RED}]})
+
+(def screen
+  {:width 800
+   :height 600})
+
+(defn trace
+  [{:keys [width height]} {:keys [objects]}]
+  {:width width
+   :height height
+   :pixels (for [x (range width)
+                       y (range height)]
+                   (let [ray (create-ray (v3 (- x (/ width 2))
+                                             (- (/ height 2) y)
+                                             0)
+                                         (v3 0 0 -1))
+                         object (find-collision ray objects)]
+                     {:x x :y y
+                      :color (if object
+                               (:color object)
+                               Color/BLACK)}))})
+
+(defn dump-trace!
+  [{:keys [width height pixels]} file-name]
+  (let [image (BufferedImage. width height BufferedImage/TYPE_4BYTE_ABGR)]
+    (doseq [pixel pixels]
+      (.setRGB image (:x pixel) (:y pixel) (.getRGB (:color pixel))))
+    (ImageIO/write image "png" (file (str file-name ".png")))))
+
+(defn trace!
+  [screen scene file-name]
+  (-> (trace screen scene) (dump-trace! file-name)))
