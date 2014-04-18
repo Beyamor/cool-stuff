b291d9f8ac02c90bdb226de1905794a522dba7fd
beyamor <beyamor@gmail.com>
2014-04-18 15:26:56 -0700

    Shape protocol

--- clj-ray-tracer/src/clj_ray_tracer/core.clj
+++ clj-ray-tracer/src/clj_ray_tracer/core.clj
@@ -24,10 +24,41 @@
   (v/sub d
          (v/scale normal (* 2 (v/dot d normal)))))
 
-(defn create-sphere
+(defprotocol Shape
+  (intersection [shape ray])
+  (normal-at-point [shape point]))
+
+(defrecord Sphere
   [center radius]
-  {:center center
-   :radius radius})
+  Shape
+  (intersection
+    [{:keys [radius] :as sphere} {:keys [direction] :as ray}]
+    (let [v (v/sub (:point ray) (:center sphere))
+          a (v/dot direction direction)
+          b (* 2 (v/dot v direction))
+          c (- (v/dot v v) (* radius radius))
+          discriminant  (- (* b b) (* 4 a c))]
+      (when-not (neg? discriminant)
+        (let [discriminant-sqrt (Math/sqrt discriminant)
+              q (if (neg? b)
+                  (/ (- (- b) discriminant-sqrt) 2)
+                  (/ (+ (- b) discriminant-sqrt) 2))
+              t0 (/ q a)
+              t1 (/ c q)
+              [t0 t1] (if (> t0 t1)
+                        [t1 t0] [t0 t1])]
+          (cond
+            (neg? t1) nil
+            (neg? t0) t1
+            :else t0)))))
+
+  (normal-at-point
+    [sphere point]
+    (-> point
+      (v/sub (:center sphere))
+      v/normalize)))
+
+(def create-sphere ->Sphere)
 
 (defn create-ray
   [point direction]
@@ -39,33 +70,6 @@
   (v/add point
          (v/scale direction t)))
 
-(defn intersection
-  [{:keys [radius] :as sphere} {:keys [direction] :as ray}]
-  (let [v (v/sub (:point ray) (:center sphere))
-        a (v/dot direction direction)
-        b (* 2 (v/dot v direction))
-        c (- (v/dot v v) (* radius radius))
-        discriminant  (- (* b b) (* 4 a c))]
-    (when-not (neg? discriminant)
-      (let [discriminant-sqrt (Math/sqrt discriminant)
-            q (if (neg? b)
-                (/ (- (- b) discriminant-sqrt) 2)
-                (/ (+ (- b) discriminant-sqrt) 2))
-            t0 (/ q a)
-            t1 (/ c q)
-            [t0 t1] (if (> t0 t1)
-                      [t1 t0] [t0 t1])]
-        (cond
-          (neg? t1) nil
-          (neg? t0) t1
-          :else t0)))))
-
-(defn normal-at-point
-  [sphere point]
-  (->
-    point
-    (v/sub (:center sphere))
-    v/normalize))
 
 (defn collision-info
   [ray {:keys [shape] :as object}]
